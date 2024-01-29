36de24819c2c86f7cad8f6e6652f3721ad575a13
Yudha Haris Purnama <yudha@clade.ventures>
2024-01-29 22:46:45 +0700

    fix(home): reminder orders

--- app/src/main/java/com/example/myreminder/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myreminder/presentation/home/HomeViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.asLiveData
 import com.example.myreminder.core.data.source.Resource
 import com.example.myreminder.core.domain.model.Reminder
 import com.example.myreminder.core.domain.usecase.ReminderUseCase
+import com.example.myreminder.core.utils.DateUtils
+import kotlinx.coroutines.flow.map
 
 class HomeViewModel(private val reminderUseCase: ReminderUseCase) : ViewModel() {
 
@@ -13,5 +15,34 @@ class HomeViewModel(private val reminderUseCase: ReminderUseCase) : ViewModel() 
         getReminder()
     }
     
-    fun getReminder(): LiveData<Resource<List<Reminder>>> = reminderUseCase.getAllReminder().asLiveData()
+    fun getReminder(): LiveData<Resource<List<Reminder>>> {
+        return reminderUseCase.getAllReminder().map {
+            when(it) {
+                is Resource.Success -> {
+                    val sorted = it.data ?: emptyList()
+                    val result = ArrayList<Reminder>()
+
+                    val active = ArrayList<Reminder>()
+                    val inactive = ArrayList<Reminder>()
+                    sorted.forEach { item ->
+                        val isPast = DateUtils.isPast(item.dateTime)
+                        if (isPast) {
+                            inactive.add(item)
+                        } else {
+                            active.add(item)
+                        }
+                    }
+                    result.addAll(active)
+                    result.addAll(inactive)
+                    Resource.Success<List<Reminder>>(result)
+                }
+                is Resource.Loading -> {
+                    Resource.Loading()
+                }
+                is Resource.Error -> {
+                    Resource.Error(it.message.toString())
+                }
+            }
+        }.asLiveData()
+    }
 }
